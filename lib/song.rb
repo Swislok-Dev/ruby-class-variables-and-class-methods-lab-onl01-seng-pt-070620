b33c9da83f7b893effc3e39321db1e4154ad5870
Cory Marker <cmarker4@gmail.com>
2020-07-27 22:50:04 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,8 +9,8 @@ class Song
     @name = name
     @artist = artist
     @genre = genre
-    @@artists << artist
-    @@genres << genre
+    @@artists << @artist
+    @@genres << @genre
     @@count += 1
   end
 
